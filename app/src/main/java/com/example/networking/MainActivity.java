3d6b69e65f9830c99e413198285d7964fa752216
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2022-04-16 12:52:51 +0200

    Add networking code

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.networking;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-public class MainActivity extends AppCompatActivity {
+    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("JsonTask", json);
     }
 }
