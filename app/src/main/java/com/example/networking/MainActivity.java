4034cffa005fb956ce2ef0558613ea23efe8484e
wilmerlundquist <b21willu@student.his.se>
2022-04-28 13:53:57 +0200

    Added ArrayList<Mountain> and RecyclerView.Adapter member variable in activity.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+
+    private ArrayList<Mountain> listItems;
+    private RecyclerView.Adapter adapter;
 
 
     @Override
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,13 @@
+package com.example.networking;
+
+public class Mountain {
+    private String title;
+
+    public Mountain(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
