a0e1d360530cbf2580eff45952d0a996ec4bcf32
b21willu <Wilmer990302>
2022-05-04 15:58:51 +0200

    Added new java class that represents an item. Added RecyclerView recyclerView member variable.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+
+    RecyclerView recyclerView;
 
 
     @Override
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,17 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private int height;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+}
